e9c2a2a68a13af1b0c409bd717421b915befe73d
jikbb2 <jikbb2@gmail.com>
2024-01-12 19:42:50 +0900

    Create: iOS project folder

---
+++ demo/demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by byeoungjik on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
